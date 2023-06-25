2bd2ac69ffdc452a111aa8b2282b951b36aa016a
MikolajP13 <mikolajp1313@gmail.com>
2023-06-25 12:18:00 +0200

    improved performance of the weather forecast between dates

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { WeatherClientService } from '../service/weather-client.service';
+import { RootWeatherBetweenDates, WeatherClientService, Daily } from '../service/weather-client.service';
 import { CityClientService, RootCities, RootCity, RootCord } from '../service/city-client.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { DateRange, MatDatepickerInputEvent } from '@angular/material/datepicker';
-import { empty } from 'rxjs';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+export interface WeatherBetweenDates {
+  time: string[];
+  weathercode: number[];
+  temperature_2m_min: number[];
+  temperature_2m_max: number[];
+  rain_sum: string[];
+  windspeed_10m_max: string[];
+}
 
 @Component({
   selector: 'app-weather',
@@ -36,12 +44,13 @@ export class WeatherComponent implements OnInit{
     }
   };
 
+  showWeather = false;
   cordsCityMap: Map<string, string> = new Map();
   searchedCities: City[] = [];
   selectedLocationCordinates: string = '';
   selectedDateRange = new FormGroup({
-    start: new FormControl<Date | null>(null),
-    end: new FormControl<Date | null>(null),
+    start: new FormControl<Date | null>(null, [Validators.required]),
+    end: new FormControl<Date | null>(null, [Validators.required]),
   });
 
   constructor(private weatherClient: WeatherClientService, private cityClient: CityClientService){
@@ -50,8 +59,43 @@ export class WeatherComponent implements OnInit{
 
   myControl = new FormControl('');
 
+  today: Date = new Date();
+  dateFlag: boolean = false;
+  maxDate: Date = new Date();
+
+  displayedColumns: string[] = ['date', 'weatherCode', 'temperature', 'rainfall', 'windspeed'];
+  recordCount: number = 0;
+  weatherForOneDay: RootWeatherBetweenDates = {
+    latitude: 0,
+    longitude: 0,
+    generationtime_ms: 0,
+    utc_offset_seconds: 0,
+    timezone: '',
+    timezone_abbreviation: '',
+    elevation: 0,
+    daily_units: {
+      time: '',
+      weathercode: '',
+      temperature_2m_max: '',
+      temperature_2m_min: '',
+      rain_sum: '',
+      windspeed_10m_max: ''
+    },
+    daily: {
+      time: [],
+      weathercode: [],
+      temperature_2m_max: [],
+      temperature_2m_min: [],
+      rain_sum: [],
+      windspeed_10m_max: []
+    }
+  };
+
+  table: WeatherBetweenDates[] = [];
+
   ngOnInit(): void {
-    
+    this.maxDate = new Date(this.today.getTime() + 14 * 24 * 60 * 60 * 1000);
+    this.today.setHours(0, 0, 0, 0);
   }
 
   onInputChange(event: any) {
@@ -76,19 +120,31 @@ export class WeatherComponent implements OnInit{
 
   show(){
     const cords = this.selectedLocationCordinates.split(',');
+    const startDate = this.selectedDateRange.value.start as Date;
+    const endDate = this.selectedDateRange.value.end as Date;
+    const numOfDaysBetweenDates = this.getDayDiff(startDate, endDate) + 1;
+    const startDateFormatted = this.formatDate(startDate);
+    const endDateFormatted = this.formatDate(endDate);
 
-    const startDate = this.formatDate(this.selectedDateRange.value.start);
-    const endDate = this.formatDate(this.selectedDateRange.value.end);
-    
-    //condition check!
-
-    this.weatherClient.getWeatherForecastBetweenDates(parseFloat(cords[0]), parseFloat(cords[1]), startDate, endDate).subscribe(
+    this.weatherClient.getWeatherForecastBetweenDates(parseFloat(cords[0]), parseFloat(cords[1]), startDateFormatted, endDateFormatted).subscribe(
       result => {
-        console.log(result.daily.temperature_2m_max);
+        for (let i = 0; i < numOfDaysBetweenDates; i++) {
+          this.recordCount++;
+          const weatherBetweenDates = {
+            time: [result.daily.time[i]],
+            weathercode: [result.daily.weathercode[i]],
+            temperature_2m_min: [result.daily.temperature_2m_min[i]],
+            temperature_2m_max: [result.daily.temperature_2m_max[i]],
+            rain_sum: [result.daily.rain_sum[i].toFixed(2)],
+            windspeed_10m_max: [result.daily.windspeed_10m_max[i].toFixed(2)]
+          };
+          this.table.push(weatherBetweenDates);
+        }
       }
     )
+    console.log(this.recordCount);
+    this.recordCount = 0;
   }
-
 
   saveCordinates(event: MatAutocompleteSelectedEvent) {
     const city: City = event.option.value;
@@ -97,6 +153,16 @@ export class WeatherComponent implements OnInit{
 
   displayFn(city: City): string {
     return city ? city.getLocationDetails().substring(0, 50) + (city.getLocationDetails().length > 50 ? '...' : '') : '';
+  }
+
+  resetForm() {
+    this.selectedDateRange.reset();
+    this.myControl.reset();
+    this.searchedCities = [];
+    this.table = [];
+    this.selectedLocationCordinates = '';
+    this.maxDate = new Date();
+    this.dateFlag = false;
   }
 
   formatDate(date: Date | null | undefined): string{
@@ -109,6 +175,51 @@ export class WeatherComponent implements OnInit{
     }
 
     return '';
+  }
+
+  isValidStartDate(): boolean {
+    const startDate = this.selectedDateRange.value.start as Date;
+    return startDate && startDate >= this.today;
+  }
+
+  isValidDateRange(): boolean {
+    const startDate = this.selectedDateRange.value.start as Date;
+    const endDate = this.selectedDateRange.value.end as Date;
+  
+    if (!startDate || !endDate) {
+      return false;
+    }
+  
+    if (startDate < this.today) {
+      return false;
+    }
+  
+    const maxDate = new Date(startDate.getTime() + 14 * 24 * 60 * 60 * 1000);
+    if (endDate > maxDate) {
+      return false;
+    }
+
+    return true;
+  }
+  
+  validateForm(): void { 
+    if (this.isValidStartDate() && this.isValidDateRange()) {
+      this.dateFlag = true;
+    } else {
+      this.dateFlag = false;
+    }
+  }
+
+  isLocationAndDateSelected(): boolean {
+    return this.dateFlag && this.selectedLocationCordinates !== null && this.selectedLocationCordinates !== '';
+  }
+
+  getDayDiff(startDate: Date, endDate: Date): number {
+    const msInDay = 24 * 60 * 60 * 1000;
+  
+    return Math.round(
+      Math.abs(Number(endDate) - Number(startDate)) / msInDay
+    );
   }
 
 }
