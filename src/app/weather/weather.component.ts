7335550302d46891a94202cfe8c1933d7d54b830
MikolajP13 <mikolajp1313@gmail.com>
2023-07-18 19:19:34 +0200

    fixed major functions, cleaned code, changed names, added comments

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { RootWeatherBetweenDates, WeatherClientService } from '../service/weather-client.service';
-import { CityClientService, RootCities, RootCord } from '../service/city-client.service';
+import {
+  RootWeatherBetweenDates,
+  WeatherClientService,
+} from '../service/weather-client.service';
+import {
+  CityClientService,
+  RootCities,
+  RootCord,
+} from '../service/city-client.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
@@ -16,9 +23,9 @@ export interface WeatherBetweenDates {
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  styleUrls: ['./weather.component.css'],
 })
-export class WeatherComponent implements OnInit{
+export class WeatherComponent implements OnInit {
   city: string = '';
   cityRoot: RootCities = [];
   cityByCord: RootCord = {
@@ -39,8 +46,8 @@ export class WeatherComponent implements OnInit{
     localityInfo: {
       LikelyLand: false,
       administrative: [],
-      informative: []
-    }
+      informative: [],
+    },
   };
 
   showWeather = false;
@@ -52,9 +59,10 @@ export class WeatherComponent implements OnInit{
     end: new FormControl<Date | null>(null, [Validators.required]),
   });
 
-  constructor(private weatherClient: WeatherClientService, private cityClient: CityClientService){
-
-  }
+  constructor(
+    private weatherClient: WeatherClientService,
+    private cityClient: CityClientService
+  ) {}
 
   myControl = new FormControl('');
 
@@ -62,7 +70,13 @@ export class WeatherComponent implements OnInit{
   dateFlag: boolean = false;
   maxDate: Date = new Date();
 
-  displayedColumns: string[] = ['date', 'weatherCode', 'temperature', 'rainfall', 'windspeed'];
+  displayedColumns: string[] = [
+    'date',
+    'weatherCode',
+    'temperature',
+    'rainfall',
+    'windspeed',
+  ];
   recordCount: number = 0;
   weatherForOneDay: RootWeatherBetweenDates = {
     latitude: 0,
@@ -78,7 +92,7 @@ export class WeatherComponent implements OnInit{
       temperature_2m_max: '',
       temperature_2m_min: '',
       rain_sum: '',
-      windspeed_10m_max: ''
+      windspeed_10m_max: '',
     },
     daily: {
       time: [],
@@ -86,8 +100,8 @@ export class WeatherComponent implements OnInit{
       temperature_2m_max: [],
       temperature_2m_min: [],
       rain_sum: [],
-      windspeed_10m_max: []
-    }
+      windspeed_10m_max: [],
+    },
   };
 
   table: WeatherBetweenDates[] = [];
@@ -100,29 +114,27 @@ export class WeatherComponent implements OnInit{
   onInputChange(event: any) {
     this.cordsCityMap = new Map();
     const enteredValue = event.target.value;
-    if(enteredValue === '')
-      this.dateFlag = false;
-    else
-      this.dateFlag = true;
-    
-    this.cityClient.getCordinatesByCityName(enteredValue).subscribe(
-      res => {
-        this.cityRoot = res;
-        const uniqueCordsSet = new Set();
-        this.cityRoot.forEach(element => {
-          const coords = `${parseFloat(element.lat).toFixed(2)},${parseFloat(element.lon).toFixed(2)}`;
-          if (!uniqueCordsSet.has(coords)) {
-            uniqueCordsSet.add(coords);
-            this.cordsCityMap.set(coords, element.display_name);
-            this.searchedCities.push(new City(coords, element.display_name));
-          }
-        });
-      }
-    )
+    if (enteredValue === '') this.dateFlag = false;
+    else this.dateFlag = true;
+
+    this.cityClient.getCordinatesByCityName(enteredValue).subscribe((res) => {
+      this.cityRoot = res;
+      const uniqueCordsSet = new Set();
+      this.cityRoot.forEach((element) => {
+        const coords = `${parseFloat(element.lat).toFixed(2)},${parseFloat(
+          element.lon
+        ).toFixed(2)}`;
+        if (!uniqueCordsSet.has(coords)) {
+          uniqueCordsSet.add(coords);
+          this.cordsCityMap.set(coords, element.display_name);
+          this.searchedCities.push(new City(coords, element.display_name));
+        }
+      });
+    });
     this.searchedCities = [];
   }
 
-  show(){
+  fillAndShowTabelWeather() {
     const cords = this.selectedLocationCordinates.split(',');
     const startDate = this.selectedDateRange.value.start as Date;
     const endDate = this.selectedDateRange.value.end as Date;
@@ -131,23 +143,27 @@ export class WeatherComponent implements OnInit{
     const endDateFormatted = this.formatDate(endDate);
     this.table = [];
 
-    this.weatherClient.getWeatherForecastBetweenDates(parseFloat(cords[0]), parseFloat(cords[1]), startDateFormatted, endDateFormatted).subscribe(
-      result => {
+    this.weatherClient
+      .getWeatherForecastBetweenDates(
+        parseFloat(cords[0]),
+        parseFloat(cords[1]),
+        startDateFormatted,
+        endDateFormatted
+      )
+      .subscribe((result) => {
         for (let i = 0; i < numOfDaysBetweenDates; i++) {
           this.recordCount++;
           const weatherBetweenDates = {
             time: [result.daily.time[i]],
-            weathercode: [result.daily.weathercode[i]],
-            temperature_2m_min: [result.daily.temperature_2m_min[i]],
-            temperature_2m_max: [result.daily.temperature_2m_max[i]],
-            rain_sum: [result.daily.rain_sum[i].toFixed(2)],
-            windspeed_10m_max: [result.daily.windspeed_10m_max[i].toFixed(2)]
+            weathercode: result.daily.weathercode[i] != null ? [result.daily.weathercode[i]] : [],
+            temperature_2m_min: result.daily.temperature_2m_min[i] != null ? [result.daily.temperature_2m_min[i]] : [],
+            temperature_2m_max: result.daily.temperature_2m_max[i] != null ? [result.daily.temperature_2m_max[i]] : [],
+            rain_sum: result.daily.rain_sum[i]  != null ? [result.daily.rain_sum[i].toFixed(2)] : ["--"],
+            windspeed_10m_max: result.daily.windspeed_10m_max[i] != null ? [result.daily.windspeed_10m_max[i].toFixed(2)] : ["--"]
           };
           this.table.push(weatherBetweenDates);
         }
-      }
-    )
-    console.log(this.recordCount);
+      });
     this.recordCount = 0;
   }
 
@@ -156,8 +172,16 @@ export class WeatherComponent implements OnInit{
     this.selectedLocationCordinates = city.getCordinates();
   }
 
+  /*
+  if the location details are longer than 50 characters, code appends an ellipsis ("...") at the end
+  if the city is null, it returns an empty string
+  it ensures that the returned string is limited to a maximum of 50 characters
+  */
   displayFn(city: City): string {
-    return city ? city.getLocationDetails().substring(0, 50) + (city.getLocationDetails().length > 50 ? '...' : '') : '';
+    return city
+      ? city.getLocationDetails().substring(0, 50) +
+          (city.getLocationDetails().length > 50 ? '...' : '')
+      : '';
   }
 
   resetForm() {
@@ -169,13 +193,18 @@ export class WeatherComponent implements OnInit{
     this.dateFlag = false;
   }
 
-  formatDate(date: Date | null | undefined): string{
+  /*
+  function takes a Date object, null or undefined as parameter and returns 
+  a formatted date string in the format yyyy-mm-dd
+  */
+  formatDate(date: Date | null | undefined): string {
     if (date) {
-    const year = date?.getFullYear();
-    const month = (date?.getMonth() + 1).toString().padStart(2, '0');
-    const day = (date?.getDate()).toString().padStart(2, '0');
+      const year = date?.getFullYear();
+      //padStart() function ensures that the number is always displayed with two digits
+      const month = (date?.getMonth() + 1).toString().padStart(2, '0');
+      const day = date?.getDate().toString().padStart(2, '0');
 
-    return `${year}-${month}-${day}`;
+      return `${year}-${month}-${day}`;
     }
 
     return '';
@@ -189,15 +218,16 @@ export class WeatherComponent implements OnInit{
   isValidDateRange(): boolean {
     const startDate = this.selectedDateRange.value.start as Date;
     const endDate = this.selectedDateRange.value.end as Date;
-  
+
+    //if the variable is null, undefined, false, 0, an empty string or NaN condition is true
     if (!startDate || !endDate) {
       return false;
     }
-  
+
     if (startDate < this.today) {
       return false;
     }
-  
+
     const maxDate = new Date(startDate.getTime() + 14 * 24 * 60 * 60 * 1000);
     if (endDate > maxDate) {
       return false;
@@ -205,8 +235,8 @@ export class WeatherComponent implements OnInit{
 
     return true;
   }
-  
-  validateForm(): void { 
+
+  validateForm(): void {
     if (this.isValidStartDate() && this.isValidDateRange()) {
       this.dateFlag = true;
     } else {
@@ -215,22 +245,23 @@ export class WeatherComponent implements OnInit{
   }
 
   isLocationAndDateSelected(): boolean {
-    return this.dateFlag && this.selectedLocationCordinates !== null && this.selectedLocationCordinates !== '';
-  }
-
-  getDayDiff(startDate: Date, endDate: Date): number {
-    const msInDay = 24 * 60 * 60 * 1000;
-  
-    return Math.round(
-      Math.abs(Number(endDate) - Number(startDate)) / msInDay
+    return (
+      this.dateFlag &&
+      this.selectedLocationCordinates !== null &&
+      this.selectedLocationCordinates !== ''
     );
   }
 
+  //function calculates the difference in days between two dates
+  getDayDiff(startDate: Date, endDate: Date): number {
+    const msInDay = 24 * 60 * 60 * 1000;
+
+    return Math.round(Math.abs(Number(endDate) - Number(startDate)) / msInDay);
+  }
 }
 
 class City {
-  constructor(private cordinates: string, private locationDetails: string){
-  }
+  constructor(private cordinates: string, private locationDetails: string) {}
 
   getCordinates(): string {
     return this.cordinates;
