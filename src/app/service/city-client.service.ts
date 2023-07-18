e7345dd56b4ff2aefc5aa639ee1a9c7e20b5250b
MikolajP13 <mikolajp1313@gmail.com>
2023-07-18 19:19:04 +0200

    cleaning and beautifying code

--- src/app/service/city-client.service.ts
+++ src/app/service/city-client.service.ts
@@ -3,78 +3,83 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class CityClientService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  public getCityNameByCordinates(latitude: number, longitude: number): Observable<RootCord>{
-    return this.httpClient.get<RootCord>(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`);
+  public getCityNameByCordinates(
+    latitude: number,
+    longitude: number
+  ): Observable<RootCord> {
+    return this.httpClient.get<RootCord>(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+    );
   }
 
-  public getCordinatesByCityName(cityName: string): Observable<RootCities>{
-    return this.httpClient.get<RootCities>(`https://geocode.maps.co/search?q=${cityName}`);
+  public getCordinatesByCityName(cityName: string): Observable<RootCities> {
+    return this.httpClient.get<RootCities>(
+      `https://geocode.maps.co/search?q=${cityName}`
+    );
   }
 }
 
 export interface RootCord {
-  latitude: number
-  longitude: number
-  continent: string
-  lookupSource: string
-  continentCode: string
-  localityLanguageRequested: string
-  city: string
-  countryName: string
-  countryCode: string
-  postcode: string
-  principalSubdivision: string
-  principalSubdivisionCode: string
-  plusCode: string
-  locality: string
-  localityInfo: LocalityInfo
+  latitude: number;
+  longitude: number;
+  continent: string;
+  lookupSource: string;
+  continentCode: string;
+  localityLanguageRequested: string;
+  city: string;
+  countryName: string;
+  countryCode: string;
+  postcode: string;
+  principalSubdivision: string;
+  principalSubdivisionCode: string;
+  plusCode: string;
+  locality: string;
+  localityInfo: LocalityInfo;
 }
 
 export interface LocalityInfo {
-  LikelyLand: boolean
-  administrative: Administrative[]
-  informative: Informative[]
+  LikelyLand: boolean;
+  administrative: Administrative[];
+  informative: Informative[];
 }
 
 export interface Administrative {
-  name: string
-  description: string
-  order: number
-  adminLevel: number
-  isoCode?: string
-  wikidataId: string
-  geonameId: number
+  name: string;
+  description: string;
+  order: number;
+  adminLevel: number;
+  isoCode?: string;
+  wikidataId: string;
+  geonameId: number;
 }
 
 export interface Informative {
-  name: string
-  description: string
-  order: number
-  isoCode?: string
-  wikidataId?: string
-  geonameId?: number
+  name: string;
+  description: string;
+  order: number;
+  isoCode?: string;
+  wikidataId?: string;
+  geonameId?: number;
 }
 
-export type RootCities = RootCity[]
+export type RootCities = RootCity[];
 
 export interface RootCity {
-  place_id: number
-  licence: string
-  powered_by: string
-  osm_type: string
-  osm_id: number
-  boundingbox: string[]
-  lat: string
-  lon: string
-  display_name: string
-  class: string
-  type: string
-  importance: number
+  place_id: number;
+  licence: string;
+  powered_by: string;
+  osm_type: string;
+  osm_id: number;
+  boundingbox: string[];
+  lat: string;
+  lon: string;
+  display_name: string;
+  class: string;
+  type: string;
+  importance: number;
 }
--- src/app/service/weather-client.service.ts
+++ src/app/service/weather-client.service.ts
@@ -3,104 +3,115 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class WeatherClientService {
   CracowLatitude: number = 50.06;
   CracowLongitude: number = 19.94;
   numberOfDays: number = 3;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  public getWeatherForecast(latitude: number, longitude: number, days: number): Observable<RootWeather>{
-    return this.httpClient.get<RootWeather>(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,rain&daily=temperature_2m_max,temperature_2m_min,rain_sum,windspeed_10m_max&current_weather=true&windspeed_unit=ms&forecast_days=${days}&timezone=auto`);
+  public getWeatherForecast(
+    latitude: number,
+    longitude: number,
+    days: number
+  ): Observable<RootWeather> {
+    return this.httpClient.get<RootWeather>(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,rain&daily=temperature_2m_max,temperature_2m_min,rain_sum,windspeed_10m_max&current_weather=true&windspeed_unit=ms&forecast_days=${days}&timezone=auto`
+    );
   }
 
-  public getWeatherForecastBetweenDates(latitude: number, longitude: number, startDate: string, endDate: string): Observable<RootWeatherBetweenDates>{ //2023-06-21
-    return this.httpClient.get<RootWeatherBetweenDates>(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum,windspeed_10m_max&windspeed_unit=ms&start_date=${startDate}&end_date=${endDate}&timezone=auto`);
+  public getWeatherForecastBetweenDates(
+    latitude: number,
+    longitude: number,
+    startDate: string,
+    endDate: string
+  ): Observable<RootWeatherBetweenDates> {
+    return this.httpClient.get<RootWeatherBetweenDates>(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum,windspeed_10m_max&windspeed_unit=ms&start_date=${startDate}&end_date=${endDate}&timezone=auto`
+    );
   }
-
 }
 
 export interface RootWeather {
-  latitude: number
-  longitude: number
-  generationtime_ms: number
-  utc_offset_seconds: number
-  timezone: string
-  timezone_abbreviation: string
-  elevation: number
-  current_weather: CurrentWeather
-  hourly_units: HourlyUnits
-  hourly: Hourly
-  daily_units: DailyUnits
-  daily: Daily
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  current_weather: CurrentWeather;
+  hourly_units: HourlyUnits;
+  hourly: Hourly;
+  daily_units: DailyUnits;
+  daily: Daily;
 }
 
 export interface CurrentWeather {
-  temperature: number
-  windspeed: number
-  winddirection: number
-  weathercode: number
-  is_day: number
-  time: string
+  temperature: number;
+  windspeed: number;
+  winddirection: number;
+  weathercode: number;
+  is_day: number;
+  time: string;
 }
 
 export interface HourlyUnits {
-  time: string
-  temperature_2m: string
-  rain: string
+  time: string;
+  temperature_2m: string;
+  rain: string;
 }
 
 export interface Hourly {
-  time: string[]
-  temperature_2m: number[]
-  rain: number[]
+  time: string[];
+  temperature_2m: number[];
+  rain: number[];
 }
 
 export interface DailyUnits {
-  time: string
-  temperature_2m_max: string
-  temperature_2m_min: string
-  rain_sum: string
-  windspeed_10m_max: string
+  time: string;
+  temperature_2m_max: string;
+  temperature_2m_min: string;
+  rain_sum: string;
+  windspeed_10m_max: string;
 }
 
 export interface Daily {
-  time: string[]
-  temperature_2m_max: number[]
-  temperature_2m_min: number[]
-  rain_sum: number[]
-  windspeed_10m_max: number[]
+  time: string[];
+  temperature_2m_max: number[];
+  temperature_2m_min: number[];
+  rain_sum: number[];
+  windspeed_10m_max: number[];
 }
 
 export interface RootWeatherBetweenDates {
-  latitude: number
-  longitude: number
-  generationtime_ms: number
-  utc_offset_seconds: number
-  timezone: string
-  timezone_abbreviation: string
-  elevation: number
-  daily_units: DailyUnitsBetweenDates
-  daily: DailyBetweenDates
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  daily_units: DailyUnitsBetweenDates;
+  daily: DailyBetweenDates;
 }
 
 export interface DailyUnitsBetweenDates {
-  time: string
-  weathercode: string
-  temperature_2m_max: string
-  temperature_2m_min: string
-  rain_sum: string
-  windspeed_10m_max: string
+  time: string;
+  weathercode: string;
+  temperature_2m_max: string;
+  temperature_2m_min: string;
+  rain_sum: string;
+  windspeed_10m_max: string;
 }
 
 export interface DailyBetweenDates {
-  time: string[]
-  weathercode: number[]
-  temperature_2m_max: number[]
-  temperature_2m_min: number[]
-  rain_sum: number[]
-  windspeed_10m_max: number[]
+  time: string[];
+  weathercode: number[];
+  temperature_2m_max: number[];
+  temperature_2m_min: number[];
+  rain_sum: number[];
+  windspeed_10m_max: number[];
 }
